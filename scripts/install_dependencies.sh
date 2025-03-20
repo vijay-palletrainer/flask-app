756b15a5dceae03da9832560ae3efaf0f71bff46
VijayaPardhaSaradhiTummala <pardhutummalagit@gmail.com>
2025-03-20 15:13:33 +0530

    changes

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y python3 python3-pip
+pip3 install -r /home/ubuntu/flask-app/requirements.txt
---
+++ scripts/start_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/flask-app
+python3 app.py > /dev/null 2>&1 &
